3571a29a493c38278097ac254cdd0dc46c8fd8f5
andrew-tawfeek <atawfeek.math@gmail.com>
2025-10-02 22:17:37 -0700

    further cleanup - jfc i'm tired and DONE

--- training/model.py
+++ training/model.py
@@ -21,7 +21,10 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import random
-from data import data_pairs
+
+from data import data_pairs  # this is the data the model will be trained on
+
+
 
 # Set random seeds for reproducibility
 # Keep this so training the model on the same dataset will produce the same outcome
